2175cf219ca63b142703a59471fcaf0127dc29ff
antsrt <an.tsaritsin@gmail.com>
2025-04-10 16:43:21 +0300

    Changed ssrl midule

--- docker/run_container.sh
+++ docker/run_container.sh
@@ -1,18 +1,19 @@
+#!/bin/bash
+
 # Allow X server connections from root user
-xhost si:localuser:root
+xhost +si:localuser:root
 
 # Run Docker container
-docker run \
-    --rm \                          # Remove container after exit
-    -it \                          # Interactive terminal
-    --ipc=host \                   # Use host's IPC namespace
-    --gpus all \                   # Enable all GPUs
-    --net=host \                   # Use host's network
-    -v ..:/workspace \              # Mount parent directory to /workspace
-    --volume=$HOME/.Xauthority:/root/.Xauthority:rw \  # X11 authentication
-    -e NVIDIA_DRIVER_CAPABILITIES=all \                 # Enable all NVIDIA capabilities
-    -e DISPLAY=$DISPLAY \          # Pass display variable
-    -v /tmp/.X11-unix:/tmp/.X11-unix \                 # X11 socket
-    --privileged \                 # Run with elevated privileges
-    ros-jax-gpu \                 # Container image name
-    bash -c "cd workspace/ssrl_ws && source devel/setup.bash && bash"  # Command to run with initial setup
+docker run --rm \
+    -it \
+    --ipc=host \
+    --gpus all \
+    --net=host \
+    -v "$(pwd)/..:/workspace" \
+    --volume="$HOME/.Xauthority:/root/.Xauthority:rw" \
+    -e NVIDIA_DRIVER_CAPABILITIES=all \
+    -e DISPLAY="$DISPLAY" \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    --privileged \
+    ssrl-ros-gpu \
+    bash -c "cd /workspace/ssrl_ws && source devel/setup.bash && bash"
--- docker/ssrl_entry.sh
+++ docker/ssrl_entry.sh
@@ -14,6 +14,10 @@ make
 sudo make install
 echo 'export LD_LIBRARY_PATH=/usr/local/lib:$LD_LIBRARY_PATH' >> ~/.bashrc
 
+cd /workspace/submodules
+cp -a unitree_ros_to_real/ ../ssrl_ws/src/
+cp -a unitree_legged_sdk/ ../ssrl_ws/src/unitree_ros_to_real/
+
 cd /workspace/ssrl_ws/src/unitree_ros_to_real/unitree_legged_sdk/
 
 rm -rf build
@@ -26,4 +30,4 @@ cd /workspace/ssrl_ws/
 catkin_make
 source devel/setup.bash
 
-
+echo "Done. Now you can start to work..."
