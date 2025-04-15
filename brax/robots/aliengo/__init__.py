db7afa5da07717d6ff01f799e63636838e579b13
antsrt <an.tsaritsin@gmail.com>
2025-04-15 18:20:30 +0300

    Bug fix

--- brax/robots/aliengo/__init__.py
+++
@@ -1,7 +0,0 @@
-"""Aliengo robot implementation."""
-
-# Export utils
-from brax.robots.aliengo.utils import AliengoUtils
-
-# Export gait
-from brax.robots.aliengo.gait import AliengoGait, AliengoGaitParams 
